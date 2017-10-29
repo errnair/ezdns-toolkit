71a852ce369dcf1bc328448ab27e24160bd93e9e
rn4ir <rn@rahul-nair.com>
2017-10-29 02:48:49 -0400

    added A record lookup

--- ezdns/dnschecker.py
+++ ezdns/dnschecker.py
@@ -20,8 +20,32 @@ def ns(url):
     nsResolver = dns.resolver.Resolver()
     nsAnswers = nsResolver.query(domain, "NS")
     for rdata in nsAnswers:
-        nameservers.append(rdata)
+        nameservers.append(str(rdata))
 
     combined_ns['whois'] = whois_ns
     combined_ns['ns'] = nameservers
     return(combined_ns)
+
+def mx(url):
+    mxservers = []
+
+    domain_parts = tldextract.extract(url)
+    domain = domain_parts.registered_domain
+
+    mxResolver = dns.resolver.Resolver()
+    mxAnswers = mxResolver.query(domain, "MX")
+    for rdata in mxAnswers:
+        mxservers.append(str(rdata))
+    return(mxservers)
+
+def a(url):
+    aRecords = []
+
+    domain_parts = tldextract.extract(url)
+    domain = domain_parts.registered_domain
+
+    aResolver = dns.resolver.Resolver()
+    aAnswers = aResolver.query(domain, "A")
+    for rdata in aAnswers:
+        aRecords.append(str(rdata))
+    return(aRecords)
