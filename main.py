a2b5a35298a3c3f935d0fc2c550cfd86aef8f9d1
rn4ir <rn@rahul-nair.com>
2017-11-01 17:50:34 -0400

    added --list lookup

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     cli_argparser.add_argument('-a', '--A', help="Get a domain's A record.", required=False)
     cli_argparser.add_argument('-mx', '--MX', help="Get a domain's MX record.", required=False)
     cli_argparser.add_argument('-txt', '--TXT', help="Get a domain's TXT record(s).", required=False)
+    cli_argparser.add_argument('-l', '--list', help="Get all the DNS record(s) of a domain.", required=False)
     cli_args = cli_argparser.parse_args()
 
     if (cli_args.NS and cli_args.A):
@@ -27,6 +28,11 @@ def main():
         print (dnschecker.mx(cli_args.MX))
     elif (cli_args.TXT):
         print (dnschecker.txt(cli_args.TXT))
+    elif (cli_args.list):
+        print (dnschecker.ns(cli_args.list))
+        print (dnschecker.a(cli_args.list))
+        print (dnschecker.mx(cli_args.list))
+        print (dnschecker.txt(cli_args.list))
     else:
         print ("Default")
 
